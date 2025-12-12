9352778a261f5726e7c1357079803281de1546e1
rohan kumar majhi <rohandevil6@gmail.com>
2025-12-12 15:26:55 +0530

    added db builder

---
+++ app/src/main/java/com/tribe7/seekho_task/data/local/db/AppDatabase.kt
@@ -0,0 +1,25 @@
+package com.tribe7.seekho_task.data.local.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.tribe7.seekho_task.data.local.dao.AnimeDao
+import com.tribe7.seekho_task.data.local.entity.AnimeEntity
+
+@Database(entities = [AnimeEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+
+    companion object{
+        private var instance: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase {
+            if(instance==null){
+                instance = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "anime.db")
+                    .build()
+            }
+            return instance!!
+        }
+    }
+
+    abstract fun animeDao(): AnimeDao
+}
